2e04450bb61bd9d12611afef024f017a6e63072e
Mykola Tokariev <mykola.tokariev@gmail.com>
2021-07-30 21:56:12 +0200

    Rating as stars

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,34 +1,39 @@
 import React, { Component } from "react";
 import firebase from "../firebase/firebase";
 
+import Rating from "@material-ui/lab/Rating";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = { first5Names: this.fetchFirst5Names() };
+    this.state = { readError: null, firs5names: [], rating: 2 };
+  }
+  async componentDidMount() {
+    try {
+      const db = firebase.database();
+      this.setState({ readError: null });
+
+      db.ref("subjects")
+        .orderByChild("rating")
+        .limitToFirst(10)
+        .on("value", (snapshot) => {
+          let firs5names = [];
+          snapshot.forEach((snap) => {
+            firs5names.push(snap.val());
+          });
+          this.setState({ firs5names });
+        });
+    } catch (error) {
+      this.setState({ readError: error.message });
+    }
   }
 
-  fetchFirst5Names = () => {
-    const resultList = [];
-    const db = firebase.database();
-    const subjectsRef = db.ref("subjects");
-    const first5names = subjectsRef.orderByChild("name").limitToLast(5);
-
-    first5names.once("value", (snapshot) => {
-      snapshot.forEach((child) => {
-        resultList.push(child.val());
-        console.log(child.val());
-      });
-    });
-
-    return resultList;
-  };
-
   render() {
-    const search = () => {};
-
     return (
       <div className="container m1">
-        <div className="input-group mb-3" onChange={search}>
+        <div className="input-group mb-3 mt-3">
           <input
             type="text"
             className="form-control"
@@ -37,14 +42,20 @@ class SearchBar extends Component {
             aria-describedby="basic-addon2"
           />
           <br />
-          <span className="input-group-text" id="basic-addon2">
-            @
-          </span>
-          <br />
         </div>
         <ul className="list-group" id="myList">
-          {this.state.first5Names.map((name) => (
-            <li>{name}</li>
+          {this.state.firs5names.map((name, indx) => (
+            <li key={indx} className="list-group-item">
+              {name["name"]}
+              <Box component="fieldset" borderColor="transparent">
+                <Rating
+                  name="read-only"
+                  value={name["rating"]}
+                  readOnly
+                  precision={0.5}
+                />
+              </Box>
+            </li>
           ))}
         </ul>
       </div>
